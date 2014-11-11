eded8f33a30266c71555adaafd3b90c868589a34
Ulric19 <karlusimon@gmail.com>
2014-11-11 18:16:53 -0400

    Update Film.h
    -Added accessor methods for private variables.
    -Added counts for each array.

--- Film.h
+++ Film.h
@@ -1,26 +1,92 @@
 class Film {
-  public:
-    Film(String description, Person* w, Person* d, Person* a, String* c, float ratings, float earnings);
-    
-    String getDescription();
-    Person* getWriter();
-    Person* getDirector();
-    Person* getProducer();
-    Person* getActors();
-    String getCharacters();
-    float getRating();
-    float getEarnings();
+public:
+Film(string description, Person* w, Person* d, Person* a, string* c, float ratings, float earnings);
+string getDescription();
+Person* getWriter();
+Person* getDirector();
+Person* getProducer();
+Person* getActors();
+string getCharacters();
+float getRating();
+float getEarnings();
+void setDescription(string desc_);
+void setWriter(Person writer_);
+void setDirector(Person director_);
+void setProducer(Person producor_);
+void setActors(Person actor_);
+void setCharacters(string);
+void setRating(float);
+void setEarnings(float);
+private:
+string description;
+Person writers[];
+int nWriters;
+Person directors[];
+int nDirectors;
+Person producers[];
+int nProducers;
+Person actors[];
+int nActors;
+string characters[];
+int nCharacters;
+float rating;
+float earnings;
+};
 
-  private:
-    MAX_PEOPLE = 10;
-    String description;
-    Person writer[MAX_PEOPLE];
-    Person directors[MAX_PEOPLE];
-    Person producers[MAX_PEOPLE];
-    Person actors[MAX_PEOPLE];
-    String characters[MAX_PEOPLE];
-    float rating;
-    float earnings;
+Film::Film(string description_, Person* writer_, Person* director_, Person* actor_, string* characters_, float ratings_, float earnings_){
 
-  }
+};
+
+string Film::getDescription(){
+    return description;
+};
+void Film::setDescription(string desc_){
+    description=desc_;
+};
+
+Person* Film::getWriter(){
+    return writers;
+};
+void Film::setWriter(Person writer_){
+    writers[nWriters++]=writer_;
+};
+
+Person* Film::getDirector(){
+    return directors;
+};
+void Film::setDirector(Person director_){
+    directors[nDirectors++]=director_;
+};
+
+Person* Film::getProducer(){
+    return producers;
+};
+void Film::setProducer(Person producer_){
+    producers[nProducers++]=producer_;
+};
+
+Person* Film::getActors(){
+    return actors;
+};
+void Film::setActors(Person actor_){
+    actors[nActors++]=actor_;
+};
+
+/*
+string* Film::getCharacters(){};
+void Film::setCharacters(string*){};
+*/
+float Film::getRating(){
+    return rating;
+};
+void Film::setRating(float rating_){
+    rating=rating_;
+};
+
+float Film::getEarnings(){
+    return earnings;
+};
+void Film::setEarnings(float earnings_){
+    earnings=earnings_;
+};
 
