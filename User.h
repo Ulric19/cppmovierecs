bf6a34313ed1583a6c78539754ef1579a0208550
Ulric19 <karlusimon@gmail.com>
2014-11-13 15:54:44 -0400

    Update User.h
    Boilerplate code

--- User.h
+++ User.h
@@ -1,14 +1,58 @@
 class User {
-  public:
-    User(String uName, String pw, Film* films, Person* people);
-    String getUserName();
-    String getPassword();
-    Film* getLikedFilms();
-    Person* getLikedPeople();
+public:
+User(string uName, string pw, Film* films, Person* people);
+User(string uName, string pw);
+string getUserName();
+string getPassword();
+Film* getLikedFilms();
+Person* getLikedPeople();
+void addLikedPerson(Person p);
+void addLikedFilm(Film f);
+void setUserName(string uName);
+void setPassword(string pw);
 
-  private:
-    String username;
-    String pasword;//Plaintext...
-    Film* likedFilms;
-    Person* likedPeople;
-}
+
+private:
+string username;
+string password;//Plaintext...
+Film* likedFilms;
+Person* likedPeople;
+int nLikedFilms;
+int nLikedPeople;
+};
+
+
+User::User(string uName, string pw){
+    username=uName;
+    password=pw;
+};
+User::User(string uName, string pw, Film* films, Person* people){
+    username=uName;
+    password=pw;
+    likedFilms=films;
+    likedPeople=people;
+};
+string User::getUserName(){
+    return username;
+};
+string User::getPassword(){
+    return password;
+};
+Film* User::getLikedFilms(){
+    return likedFilms;
+};
+Person* User::getLikedPeople(){
+    return likedPeople;
+};
+void User::addLikedPerson(Person p){
+    likedPeople[nLikedPeople++];
+};
+void User::addLikedFilm(Film f){
+    likedFilms[nLikedPeople++];
+};
+void User::setUserName(string uName){
+    username=uName;
+};
+void User::setPassword(string pw){
+    password=pw;
+};
