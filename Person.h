5c31a1371209f25e15063c10a11ddd559a8631b9
Ulric19 <karlusimon@gmail.com>
2014-11-13 20:28:54 -0400

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,53 +1,19 @@
 class Person{
 public:
-Person(Film contrubutedTo[], string bio, Gender gender, int activeSince);
-Film* getContributedTo();
-void addContributedTo(Film* film);
-string getBio();
-void setBio(string s);
-Gender getGender();
-void setGender(enum Gender);
-int getActiveSince();
-void setActiveSince(int y);
+    Person(Film contrubutedTo[], std::string bio, Gender gender, int activeSince);
+    Film* getContributedTo();
+    void addContributedTo(Film* film);
+    std::string getBio();
+    void setBio(std::string s);
+    Gender getGender();
+    void setGender(enum Gender);
+    int getActiveSince();
+    void setActiveSince(int y);
 private:
-Film contributedTo[];
-int numberContributedTo; //Unsure of this?
-string bio;
-Gender gender;
-int activeSince;
+    Film contributedTo[];
+    int numberContributedTo; //Unsure of this?
+    std::string bio;
+    Gender gender;
+    int activeSince;
 };
 
-/*
-Person::Person(Film contrubutedTo[], string bio, Gender gender, int activeSince){
-    
-};
-*/
-void Person::setGender(Gender g){
-    gender = g;
-};
-Gender Person::getGender(){
-    return gender;
-};
-
-void Person::setBio(string s){
-   bio=s;
-};
-string Person::getBio(){
-    return bio;
-};
-
-void Person::setActiveSince(int as){
-   activeSince=as;
-};
-int Person::getActiveSince(){
-    return activeSince;
-};
-
-Film* Person::getContributedTo(){
-    return contributedTo;
-};
-/*
-void Person::addContributedTo(Film* film){
-    contributedTo[numberContributedTo++]=film;
-}
- * */
