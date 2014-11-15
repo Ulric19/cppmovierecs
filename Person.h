92a4eefbde726b3da3b8ffcf5407612254276dbb
Ulric19 <karlusimon@gmail.com>
2014-11-15 17:14:41 -0400

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,26 +1,26 @@
-
-#ifndef FILM_H
-#define FILM_H
-#include "Film.h"
-#endif FILM_H /*  */
+    /* 
+ * File:   Person.h
+ * Author: Karl
+ *
+ * Created on November 8, 2014, 2:37 PM
+*/
 
 #ifndef PERSON_H
 #define	PERSON_H
-
-class Person {
+//Resolves errors around Film not being detected//
+class Film;
+#include <string>
+class Person{
 public:
-    Person(std::string name, Film* contrubutedTo, int activeSince);
-    Film* getContributedTo();
-    void addContributedTo(Film* film);
+    Person(std::string name,Film* cT, int aS);
+    Film* getFilms();
+    void addFilm(Film*);
     int getActiveSince();
     void setActiveSince(int y);
 private:
     std::string name;
-    Film contributedTo[];
+    Film* contributedTo[];
     int activeSince;
 };
 
 #endif	/* PERSON_H */
-
-
-
