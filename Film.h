5ea96baed3e513fa95f3c254ecb58748532aea94
Ulric19 <karlusimon@gmail.com>
2014-11-15 16:29:52 -0400

    Update Film.h

--- Film.h
+++ Film.h
@@ -1,45 +1,42 @@
-
 #ifndef FILM_H
 #define	FILM_H
+#include "Person.h"
 
+
+enum type{comedy, tradgedy};
 class Film {
 public:
-Film(std::string description, Person* w, Person* d, Person* a, std::string* c, float ratings, float earnings);
-std::string getDescription();
-Person* getWriter();
-    Person* getDirector();
-    Person* getProducer();
-    Person* getActors();
-    std::string getCharacters();
-    float getRating();
-    float getEarnings();
+    Film(std::string t, Person d, Person actor, Person actress, float rating, int mpaa, int released, type g, int a );
+    std::string getTitle();
+    void setTitle(std::string name_);
+    Person getDirector();
+    void setDirector(Person);
+    Person getActor();
+    void setActor(Person actor_);
+    Person getActress();
+    void setActress(Person actress_);
+    int getRating();
+    void setRating(int);
     int getYearReleased();
-    void setDescription(std::string desc_);
-    void setWriter(Person writer_);
-    void setDirector(Person director_);
-    void setProducer(Person producor_);
-    void setActors(Person actor_);
-    void setCharacters(std::string);
-    void setRating(float);
-    void setEarnings(float);
     void setYearReleased(int);
+    int getMPAA();
+    void setMPAA(int);
+    type getGenre();
+    void setGenre(type);
+    int getAwards();
+    void setAwards(int);
+    
 private:
-    std::string description;
-    Person writers[];
-    int nWriters;
-    Person directors[];
-    int nDirectors;
-    Person producers[];
-    int nProducers;
-    Person actors[];
-    int nActors;
-    std::string characters[];
-    int nCharacters;
-    float rating;
-    float earnings;
+    std::string title;
+    Person director;
+    Person actor;
+    Person actress;
+    int rating;
+    int mpaa;
     int yearReleased;
+    type genre;
+    int awards;
 };
 
 
 #endif	/* FILM_H */
-
