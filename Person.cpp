16786afc96611c2cb5c1dc0c6d51f2a749726c4d
Ulric19 <karlusimon@gmail.com>
2014-11-15 17:14:24 -0400

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -2,11 +2,11 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 class Person{
 public:
+    Person();
     Person(string name,Film contrubutedTo[], int activeSince);
     Film* getFilms();
     void addFilm(Film* film);
@@ -14,10 +14,15 @@ public:
     void setActiveSince(int y);
 private:
     string name;
+    //ADT of Films. Array of some size makes sense?
     Film contributedTo[];
     int activeSince;
 };
-
+Person::Person(){
+    name="";
+    //contributedTo[]=NULL;
+    activeSince=0;
+};
 Person::Person(string name,Film contrubutedTo[], int activeSince) : name(name), contributedTo(contributedTo), activeSince(activeSince){
 };
 
@@ -26,14 +31,13 @@ void Person::setActiveSince(int as){
     activeSince=as;
 };
 int Person::getActiveSince(){
-return activeSince;
+    return activeSince;
 };
-Film* Person::getContributedTo(){
-return contributedTo;
-};
+/*
 void Person::addFilm(Film* f){
-    
+    contributedTo+=f;
 };
+*/
 Film* Person::getFilms(){
-    
+    return contributedTo;
 };
