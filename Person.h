434b2a8d067e32eaadd778653a5180ed788cc320
Ulric19 <karlusimon@gmail.com>
2014-11-15 16:31:04 -0400

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,33 +1,26 @@
 
-#ifndef PERSON_H
-#define	PERSON_H
-#include "Person.h"
-#endif	/* PERSON_H */
-
-
 #ifndef FILM_H
 #define FILM_H
 #include "Film.h"
-#endif //FILM_H
+#endif FILM_H /*  */
 
-class Person{
+#ifndef PERSON_H
+#define	PERSON_H
+
+class Person {
 public:
-    Person(std::string bio, sex gender, int activeSince);
+    Person(std::string name, Film* contrubutedTo, int activeSince);
     Film* getContributedTo();
     void addContributedTo(Film* film);
-    std::string getBio();
-    void setBio(std::string s);
-    sex getSex();
-    void setSex(enum sex);
     int getActiveSince();
     void setActiveSince(int y);
 private:
-    std::list<Film*> contributedTo;
-    int nContributedTo; //Unsure of this?
-    std::string bio;
-    sex ex;
+    std::string name;
+    Film contributedTo[];
     int activeSince;
 };
+
+#endif	/* PERSON_H */
 
 
 
