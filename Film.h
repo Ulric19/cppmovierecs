ccc54367c5f923c1af52c2a2b45b119435dab7c1
Ulric19 <karlusimon@gmail.com>
2014-11-16 23:45:32 -0400

    Update Film.h
    corresponds to .cpp

--- Film.h
+++ Film.h
@@ -1,43 +1,52 @@
+
+
 #ifndef FILM_H
 #define	FILM_H
 #include <string>
+#include <vector>
+using namespace std;
+const int MAX_PEOPLE=100;
+enum type{comedy, horror, action, drama, mystery, science_fiction, music, war, western,undefined};
+enum rating{G, PG, PG_13, R, NC_17, not_specified};
 class Person;
 
-enum type{comedy, tradgedy};
 class Film {
+
 public:
-    Film(std::string t, Person d, Person actor, Person actress, float rating, int mpaa, int released, type g, int a );
-    std::string getTitle();
-    void setTitle(std::string name_);
-    Person getDirector();
-    void setDirector(Person);
-    Person getActor();
-    void setActor(Person actor_);
-    Person getActress();
-    void setActress(Person actress_);
-    int getRating();
-    void setRating(int);
-    int getYearReleased();
-    void setYearReleased(int);
-    int getMPAA();
-    void setMPAA(int);
-    type getGenre();
-    void setGenre(type);
-    int getAwards();
-    void setAwards(int);
-    
+Film();
+Film(std::string, Person*, Person*, Person*, float, int, int, type, int);
+std::string getTitle();
+void setTitle(const std::string& name);
+Person* getDirector();
+void setDirector(Person*);
+void setDirector(Person);
+Person* getActor();
+void setActor(Person*);
+void setActor(Person);
+Person* getActress();
+void setActress(Person*);
+void setActress(Person);
+int getRating();
+void setRating(int);
+int getYearReleased();
+void setYearReleased(int);
+int getMPAA();
+void setMPAA(int);
+type getGenre();
+void setGenre(type);
+int getAwards();
+void setAwards(int);
+
 private:
-    std::string title;
-    Person* director;
-    Person* actor;
-    Person* actress;
-    int rating;
-    int mpaa;
-    int yearReleased;
-    type genre;
-    int awards;
+std::string title;
+Person* director;
+Person* actor;
+Person* actress;
+int rating;
+int mpaa;
+int yearReleased;
+type genre;
+int awards;
 };
 
-
 #endif	/* FILM_H */
-
