2ef4dbe3d4b0c414a11a6c8d98b8c03d426f86df
Ulric19 <karlusimon@gmail.com>
2014-11-16 23:44:56 -0400

    Update Film.cpp
    Implemented Persons correctly.
    All mutators/accessors are functional

--- Film.cpp
+++ Film.cpp
@@ -1,25 +1,31 @@
-#include "Person.h"
+
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Person.h"
+
 using namespace std;
-const int MAX_PEOPLE=100;
 enum type{comedy, horror, action, drama, mystery, science_fiction, music, war, western,undefined};
 enum rating{G, PG, PG_13, R, NC_17, not_specified};
+
+
 class Film {
+
 public:
 Film();
-Film(string t, Person* d, Person* actor, Person* actress, float rating, int mpaa, int released, type g, int a );
-string getTitle();
-void setTitle(string name_);
+Film(std::string, Person*, Person*, Person*, float, int, int, type, int);
+std::string getTitle();
+void setTitle(const std::string& name);
 Person* getDirector();
 void setDirector(Person*);
+void setDirector(Person);
 Person* getActor();
-void setActor(Person *actor_);
-void setActor(Person actor_);
+void setActor(Person*);
+void setActor(Person);
 Person* getActress();
-void setActress(Person* actress_);
+void setActress(Person*);
+void setActress(Person);
 int getRating();
 void setRating(int);
 int getYearReleased();
@@ -30,8 +36,9 @@ type getGenre();
 void setGenre(type);
 int getAwards();
 void setAwards(int);
+
 private:
-string title;
+std::string title;
 Person* director;
 Person* actor;
 Person* actress;
@@ -41,6 +48,7 @@ int yearReleased;
 type genre;
 int awards;
 };
+
 Film::Film(){
     title="";
     director=NULL;
@@ -53,38 +61,50 @@ Film::Film(){
 };
 Film::Film(string t, Person *d, Person *actor_, Person *actress_, float rating, int mpaa, int released, type g, int a ): title(t), director(d), actor(actor_), actress(actress_), rating(rating), mpaa(mpaa), yearReleased(released), genre(g), awards(a){};
 string Film::getTitle(){
-return title;
-}
-void Film::setTitle(string n){
     cout<<"\nTitle: "<<title;
+    return title;
+}
+void Film::setTitle(const std::string& n){
+
 title=n;
 }
+
 Person* Film::getDirector(){
     cout<<"\nDirector: "<<director->getName();
     return director;
 };
-void Film::setDirector(Person *director_){
+void Film::setDirector( Person *director_){
 director=director_;
 };
+void Film::setDirector( Person director_){
+director=&director_;
+};
+
 Person* Film::getActor(){
     cout<<"\nLead Actor: "<<actor->getName();
     return actor;
 };
+
 void Film::setActor(Person *actor_){
     cout<<actor_->getName();
     actor=actor_;
 };
 void Film::setActor(Person actor_){
-  //  cout<<actor_.getName();
-   // actor=&actor_;
+    cout<<actor_.getName();
+    actor=&actor_;
 };
+
 Person* Film::getActress(){
         cout<<"\nLead Actress: "<<actress->getName();
 return actress;
 };
 void Film::setActress(Person *actress_){
 actress=actress_;
-}
+};
+void Film::setActress(Person actress_){
+actress=&actress_;
+};
+
 int Film::getRating(){
         cout<<"\nAverage Review: "<<rating;
 return rating;
@@ -92,13 +112,15 @@ return rating;
 void Film::setRating(int rating_){
 rating=rating_;
 };
+
 int Film::getMPAA(){
         cout<<"\nRated: "<<mpaa;
 return mpaa;
-}
+};
 void Film::setMPAA(int m){
 mpaa=m;
-}
+};
+
 type Film::getGenre(){
         cout<<"\nGenre: "<<genre;
 return genre;
@@ -106,6 +128,7 @@ return genre;
 void Film::setGenre(type g){
 genre=g;
 };
+
 int Film::getAwards(){
         cout<<"\nNumber of Awards: "<<awards;
 return awards;
