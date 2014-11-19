1460e64e735da26e8c57094bd2b24f1f01df8231
Ulric19 <karlusimon@gmail.com>
2014-11-19 13:47:03 -0400

    Update User.h

--- User.h
+++ User.h
@@ -13,7 +13,7 @@ std::string getPassword();
 void setPassword(std::string pw);
 std::vector<Film*> getLikedFilms();
 std::vector<Person*> getLikedPeople();
-std::vector<type> getLikedGenres();
+std::vector<std::string> getLikedGenres();
 void addLikedFilm(Film*);
 int scoreFilm(Film*);
 
@@ -21,7 +21,7 @@ private:
 std::string username;
 std::string password;//Plaintext...
 std::vector<Film*> filmArray;
-std::vector<type> genreArray;
+std::vector<std::string> genreArray;
 std::vector<Person*> personArray;//Needs to be populated from the liked films array
 int nLikedGenres;
 int nLikedFilms;
