b13034f9554bd0c66844933082214262d3cbad57
Ulric19 <karlusimon@gmail.com>
2014-11-23 20:16:57 -0400

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include "Film.h"
 #include "Person.h"
+
 using namespace std;
 class User{
 
@@ -20,7 +21,7 @@ std::vector<Film*> getLikedFilms();
 std::vector<Person*> getLikedPeople();
 std::vector<std::string> getLikedGenres();
 void addLikedFilm(Film*);
-int scoreFilm(Film*);
+float scoreFilm(Film*);
 int checkVector(vector<User*> a);
 private:
 std::string username;
@@ -28,6 +29,8 @@ std::string password;//Plaintext...
 std::vector<Film*> filmArray;
 std::vector<std::string> genreArray;
 std::vector<Person*> personArray;//Needs to be populated from the liked films array
+int checkLikedGenre(std::string a);
+vector<Film*> sortFilms(vector<Film*> f);
 int nLikedGenres;
 int nLikedFilms;
 int nLikedPeople;
@@ -83,78 +86,134 @@ void User::addLikedFilm(Film* f){
     int isThere=-1;
     isThere=f->checkVector(filmArray);
         if(isThere==-1){
-            cout<<"\nFilm has not yet been liked by user";
+            //cout<<"\nFilm has not yet been liked by user";
             filmArray.push_back(f);
             nLikedFilms++;
         }
         else{
-            cout<<"\nFilm already liked by user";
+            //cout<<"\nFilm already liked by user";
         }
 //add people
     isThere=f->actor->checkVector(personArray);
         if(isThere==-1){
-            cout<<"\nNot yet liked by user(Person)";
+            //cout<<"\nNot yet liked by user(Person)";
             personArray.push_back(f->actor);
             nLikedPeople++;
         }
         else{
-            cout<<"\nPerson already liked by user";
+            //cout<<"\nPerson already liked by user";
             
         }
     isThere=f->actress->checkVector(personArray);
         if(isThere==-1){
-            cout<<"\nNot yet liked by user(Person)";
+            //cout<<"\nNot yet liked by user(Person)";
             personArray.push_back(f->actress);
             nLikedPeople++;
         }
         else{
-            cout<<"\nPerson already liked by user";
+            //cout<<"\nPerson already liked by user";
             
         }
     isThere=f->director->checkVector(personArray);
     if(isThere==-1){
-        cout<<"\nNot yet liked by user(Person)";
+        //cout<<"\nNot yet liked by user(Person)";
         personArray.push_back(f->director);
         nLikedPeople++;
     }
     else{
-            cout<<"\nPerson already liked by user";
+        //cout<<"\nPerson already liked by user";
             
         }
-personArray.push_back(f->director);
-nLikedPeople++;
-personArray.push_back(f->actress);
-nLikedPeople++;
-//add genre
-genreArray.push_back(f->getGenre());
-nLikedGenres++;
+    isThere=checkLikedGenre(f->getGenre());
+    if(isThere==-1){
+        //cout<<"\nNot yet liked by user(Genre)";
+        genreArray.push_back(f->getGenre());
+        nLikedGenres++;
+    }
+    else{
+        //cout<<"\nGenre already liked by user";
+        }
 };
 //Not working at the moment...
-int User::scoreFilm(Film* f){
-int total=1;
-int counter=0;
-while(counter<nLikedFilms){
-    if(f->director->getName()==personArray[counter]->getName()||f->actress->getName()==personArray[counter]->getName()||f->actor->getName()==personArray[++counter]->getName()){
-        total+=5;
+float User::scoreFilm(Film* f){
+    //instead of using a base of 1, any film gets a base score of its score
+    float total=f->getRating();
+    if(total==0){
+        total=1;
     }
-}
-counter=0;
-while(counter<nLikedGenres){
-    if(f->getGenre()==(genreArray[++counter])){
-        total+=10;
+    int counter=0;
+    //cout<<"Number of liked Films== "<<nLikedFilms<<"\nNumber of liked "<<nLikedPeople;
+    while(counter<nLikedPeople){
+        if(f->director->getName()==personArray[counter]->getName()||f->actress->getName()==personArray[counter]->getName()||f->actor->getName()==personArray[counter]->getName()){
+            total+=10;
+        }
+        counter++;
     }
-}
-total=(total+pow(f->getRating(), 1.5));
-total=(pow(total, 1.6*(f->getAwards()+1)));
-return total;
+    //This process needs to be fine tuned...
+    counter=0;
+    while(counter<nLikedGenres){
+        if(f->getGenre()==(genreArray[counter])){
+            total+=15;
+        }
+        counter++;
+    }
+    total=(total+pow(total, 2));
+    total=(pow(total, 1.6*(f->getAwards()+1)));
+    return total;
 };
 
 
 int User::checkVector(vector<User*> a){
     int i=0;
     while(i<a.size()){
-        if(a[i]->getUserName()==this->getUserName()) return i;
+        if(a[i]->getUserName()==this->getUserName()){
+            return i;
+        }
         i++;
     }
     return -1;
+};
+int User::checkLikedGenre(std::string a){
+    int i=0;
+    while(i<genreArray.size()){
+        if(a==genreArray[i]){
+            return i;   
+        }
+        i++;
+    }
+    return -1;
+};
+
+//Method designed to take sort a vector array based on the scoreFilm() method.
+//I HOPE THIS WORKS AS IT SHOULD... Hardly TESTED. It seemed to sort them in desc
+//order, which is all I want. This is the last method I made, and most likely to
+//be broken. 
+vector<Film*> User::sortFilms(vector<Film*> f){
+    vector<Film*> sortedFilms;
+    int i=0;
+    for(i;i<f.size();i++){
+        int j=0;
+        if(sortedFilms.empty()){
+                sortedFilms.push_back(f[i]);
+                //cout<<scoreFilm(f[i]);
+                i++;
+                continue;
+        }
+        //cout<<"\n"<<sortedFilms.size();
+        while(j<sortedFilms.size()){
+            float rating=scoreFilm(f[i]);
+            if(rating>scoreFilm(sortedFilms[j])||rating==scoreFilm(sortedFilms[j])){
+                //cout<<"\nInserting "<<rating<<" at index "<<j;
+                sortedFilms.insert(sortedFilms.begin()+j, f[i]);
+                break;
+            }
+            else{
+                j++;
+            }
+        }
+        //cout<<scoreFilm(f[i]);
+        sortedFilms.push_back(f[i]);
+    }
+    return sortedFilms;
+    
 };
